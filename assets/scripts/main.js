fd889677fdb0eefc093a140da179102c27ae61c1
Mihas <32958803+MrMihas@users.noreply.github.com>
2021-05-25 19:32:12 +0300

    without responsive and footer, feedback, news

---
+++ assets/scripts/main.js
@@ -0,0 +1,20 @@
+let swiper = new Swiper(".history__slider", {
+    slidesPerView: "auto",
+        
+        pagination: {
+          el: ".swiper-pagination",
+          clickable: true,
+          dynamicBullets: true,
+    },
+  });
+
+  let swiper2 = new Swiper(".management__slider", {
+    slidesPerView: "auto",
+
+        
+        pagination: {
+          el: ".swiper-pagination2",
+          clickable: true,
+          dynamicBullets: true,
+    },
+  });
